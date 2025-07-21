f1ae5024776ca15fc5dabf42df3261e7453f7562
Taiwo Oladamola <taiwoluke@gmail.com>
2025-07-21 19:24:57 +0100

    Frontend setup with the Navigation file

---
+++ FrontEnd/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+const Nav = () => {
+
+  return (
+    <nav>
+        <img src="" alt="logo" />
+
+        <ul>
+            <li><a href="">Femme_Fantastica</a></li>
+            <li><a href="">Daily_Tips</a></li>
+            <li><a href="">Woman_to_Woman</a></li>
+            <li><a href="">Femmeposia</a></li>
+            <li><a href="">Menstral_Note</a></li>
+            <li><a href="">Editions</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
