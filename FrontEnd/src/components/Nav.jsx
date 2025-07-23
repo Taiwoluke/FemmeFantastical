4b014ccbe5c1005d82ac08bc438e41da0f5a94a5
Taiwo Oladamola <taiwoluke@gmail.com>
2025-07-23 17:37:58 +0100

    setup the react-router and created the navigation pane

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -1,18 +1,20 @@
 import React from 'react' 
+import Logo from '../assets/ff_light.png'
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
 
   return (
-    <nav>
-        <img src="" alt="logo" />
+    <nav className='bg-black flex flex-row'>
+        <img src={Logo} alt="logo" width={'20%'} height={'20%'} />
 
-        <ul className='flex flex-row'>
-            <li><a href="">Femme_Fantastica</a></li>
-            <li><a href="">Daily_Tips</a></li>
-            <li><a href="">Woman_to_Woman</a></li>
-            <li><a href="">Femmeposia</a></li>
-            <li><a href="">Menstrual_Note</a></li>
-            <li><a href="">Editions</a></li>
+        <ul className='flex flex-row justify-center gap-8 m-auto text-white nav-text'>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">FemmeFantastica</a></li>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">DailyTips</a></li>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">WomanToWoman</a></li>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">FemmePosia</a></li>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">MenstrualNote</a></li>
+            <li className='hover:text-pink-200 transition-colors ease-linear'><a href="">Editions</a></li>
         </ul>
     </nav>
   )
